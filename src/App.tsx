ed8b2e3f7034a94f2663741675ed66ab3a35ea9f
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2024-09-18 14:32:04 -0500

    Added app title

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import styles from "./App.module.scss";
 function App() {
   return (
     <div className={`${styles.app}`}>
+      <h1>MY OUTER WILDS MUSIC BOX</h1>
       <div className={`${styles.cardsContainer}`}>
         {mockTravelerList.map((traveler) => (
           <TravelerCell traveler={traveler} />
